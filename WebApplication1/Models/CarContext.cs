1fa807a07a1201371ac29e578598fc9d9fe903c5
adamlegowski0039 <64014498+adamlegowski0039@users.noreply.github.com>
2021-06-09 14:01:29 +0200

    Add files via upload

---
+++ WebApplication1/Models/CarContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CarContext : DbContext
+
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        public CarContext(DbContextOptions<CarContext> options) : base(options)
+        { }
+    }
+}
