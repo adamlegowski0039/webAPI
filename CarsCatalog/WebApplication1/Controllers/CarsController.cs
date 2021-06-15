ff106bb72266ebafb3514a19c775225587c62f4f
adamlegowski0039 <64014498+adamlegowski0039@users.noreply.github.com>
2021-06-15 14:21:47 +0200

    Add files via upload

---
+++ CarsCatalog/WebApplication1/Controllers/CarsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly CarContext _context;
+
+        public CarsController(CarContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Car> GetCars()
+        {
+            return _context.Cars.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Car> GetCar(int id)
+        {
+            var Car = _context.Cars.Find(id);
+
+            if (Car == null)
+            {
+                return NotFound();
+            }
+
+            return Car;
+        }
+
+        [HttpPost]
+        public ActionResult<Car> PostCar(Car car)
+        {
+            _context.Cars.Add(car);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetCar), new { id = car.Id }, car);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutCar(int id, Car car)
+        {
+            if (id != car.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Cars.Update(car);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCar(int id)
+        {
+            var Car = _context.Cars.Find(id);
+
+            if (Car == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cars.Remove(Car);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ CarsCatalog/WebApplication1/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Producer { get; set; }
+        public string Model { get; set; }
+        public string Capacity { get; set; }
+        public int Power { get; set; }
+
+
+    }
+}
